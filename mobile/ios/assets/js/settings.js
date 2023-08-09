0aaa93a78b4f007383c664ef81a39d827b675e46
Dylan Clayton <dylanclayton@Brendas-Macbook-Pro.local>
2023-08-09 17:22:40 -0400

    add IOS and android folders

--- mobile/ios/assets/js/settings.js
+++ mobile/ios/assets/js/settings.js
@@ -3,47 +3,51 @@ function dzThemeSettings()
 	var dzThemeSettings = '<ul class="theme-color-settings"><li><input class="filled-in" id="primary_color_1" name="primary_bg" type="radio" value="color-red" /><label for="primary_color_1"></label></li><li><input class="filled-in" id="primary_color_2" name="primary_bg" type="radio" value="color-green" /> <label for="primary_color_2"></label></li><li><input class="filled-in" id="primary_color_3" name="primary_bg" type="radio" value="color-blue" /> <label for="primary_color_3"></label></li><li><input class="filled-in" id="primary_color_4" name="primary_bg" type="radio" value="color-pink" /> <label for="primary_color_4"></label></li><li><input class="filled-in" id="primary_color_5" name="primary_bg" type="radio" value="color-yellow" /> <label for="primary_color_5"></label></li><li><input class="filled-in" id="primary_color_6" name="primary_bg" type="radio" value="color-orange" /> <label for="primary_color_6"></label></li><li><input class="filled-in" id="primary_color_7" name="primary_bg" type="radio" value="color-purple" /> <label for="primary_color_7"></label></li><li><input class="filled-in" id="primary_color_8" name="primary_bg" type="radio" value="color-deeppurple" /> <label for="primary_color_8"></label></li><li><input class="filled-in" id="primary_color_9" name="primary_bg" type="radio" value="color-lightblue" /> <label for="primary_color_9"></label></li><li><input class="filled-in" id="primary_color_10" name="primary_bg" type="radio" value="color-teal" /> <label for="primary_color_10"></label></li><li><input class="filled-in" id="primary_color_11" name="primary_bg" type="radio" value="color-lime" /> <label for="primary_color_11"></label></li><li><input class="filled-in" id="primary_color_12" name="primary_bg" type="radio" value="color-deeporange" /> <label for="primary_color_12"></label></li></ul>';
 }
 
+/* Theme Panel Save */
+var themeOption = ['themeColor', 'themeVersion'];
 
-    /* Theme Panel Save */
-	var themeOption = ['themeColor','themeVersion'];
-	const body = $('body');
-    const html = $('html');
+(function ($) {
+    "use strict";
+    dzThemeSettings();
 
-(function($) {
-    "use strict"
-	dzThemeSettings();
-
-    //get the DOM elements from right sidebar
+    // get the DOM elements from right sidebar
     const versionSelect = $('#theme_version');
+    const themeButton = $('.theme-btn');
+    const colorInputs = $('input[name="theme_color"]');
 
-    //change the theme version controller
-    /* versionSelect.on('change', function() {
-        body.attr('data-theme-version', this.value);
-		setCookie('themeVersion_value', this.value);
-    }); */
-	
-    //change the theme version controller
-    jQuery('.theme-btn').on('click',function(){
-        jQuery('body').toggleClass('theme-dark');
-        jQuery('.theme-btn').toggleClass('active');
-        if(jQuery('body').hasClass('theme-dark')){
-           setCookie('themeVersion_value', 'theme-dark'); 
-        }else{
-           setCookie('themeVersion_value', '');  
-        }
-        
+    // retrieve dark mode preference from localStorage
+    const darkModePreference = localStorage.getItem('darkMode');
+
+    // apply dark mode preference if set to true
+    if (darkModePreference === 'true') {
+        $('body').addClass('theme-dark');
+        themeButton.addClass('active');
+    }
+
+    // change the theme version controller
+    versionSelect.on('change', function () {
+        const themeVersionValue = this.value;
+        body.attr('data-theme-version', themeVersionValue);
+        setCookie('themeVersion_value', themeVersionValue);
     });
-    
-	//change the primary color controller
-    $('input[name="theme_color"]').on('click', function() {
-        body.attr('data-theme-color', this.value);
-		setCookie('themeColor_value', this.value);
+
+    // toggle dark mode on theme button click
+    themeButton.on('click', function () {
+        $('body').toggleClass('theme-dark');
+        themeButton.toggleClass('active');
+
+        // store dark mode preference in localStorage
+        const isDarkMode = $('body').hasClass('theme-dark');
+        localStorage.setItem('darkMode', isDarkMode);
     });
-    
-    
-    /* Set Theme By Cookie */
-    setThemePanel();
-	
+
+    // change the primary color controller
+    colorInputs.on('click', function () {
+        const themeColorValue = this.value;
+        body.attr('data-theme-color', themeColorValue);
+        setCookie('themeColor_value', themeColorValue);
+    });
+
 })(jQuery);
 
 
